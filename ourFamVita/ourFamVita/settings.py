d33b84a9622ddaa33fe43a78b6b2ff1a259d19ba
Ubuntu <ubuntu@ip-172-31-26-106.ap-northeast-1.compute.internal>
2024-04-15 23:15:10 +0900

    feat: connect mysql

--- ourFamVita/ourFamVita/settings.py
+++ ourFamVita/ourFamVita/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,10 +78,20 @@ WSGI_APPLICATION = 'ourFamVita.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ourFamVitaDB',
+        'USER': 'admin',
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
