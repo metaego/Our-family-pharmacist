898287f0d5cc039dcbb048d12d09d58de1acafb7
sapjil <ksy7678@gmail.com>
2024-04-24 20:53:36 +0900

    feat: update main page backend logic and recom_profile_info page backend logic

--- ourFamVita/ourFamVita/settings.py
+++ ourFamVita/ourFamVita/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'home',
     'recommends',
     'products',
+    'ourFamVita',
 ]
 
 MIDDLEWARE = [
--- ourFamVita/recommends/urls.py
+++ ourFamVita/recommends/urls.py
@@ -12,21 +12,21 @@ urlpatterns = [
     # AI추천받기: 영양 성분 리포트
     # menu: ai 영양제 추천받기(profile_info) > 영양 성분 리포트
     # /recommends/{profile-id}/surveys/{survey-id}
-    path('profile-id/surveys/survey-id/', views.recom_profile_total_report, name='profile_total_report'),
+    path('profile_id/surveys/survey_id/', views.recom_profile_total_report, name='profile_total_report'),
     
     # AI추천받기: 영양제 추천 목록(추천받은 영양 성분 기반)
     # menu: ai 영양제 추천받기(profile_info) > 영양 성분 리포트 > 영양제 추천 목록 
     # /recommends/{profile-id}/surveys/{survey-id}/rec-nut-products/{nutri-num}
-    path('profile-id/surveys/survey-id/rec-nut-products/nutri-num/', views.recom_products_nutri_base, name='products_nutri_base'),
+    path('profile_id/surveys/survey_id/rec_nut_products/nutri_num/', views.recom_products_nutri_base, name='products_nutri_base'),
     
     # AI추천받기: 영양제 추천 목록(영양 성분 리포트 기반)
     # menu: ai 영양제 추천받기(profile_info) > 영양 성분 리포트 > 영양제 추천 목록 
     # /recommends/{profile-id}/surveys/{survey-id}/rec-total-products/
-    path('profile-id/surveys/survey-id/rec-total-products/', views.recom_products_profile_base, name='products_profile_base'),
+    path('profile_id/surveys/survey_id/rec_total_products/', views.recom_products_profile_base, name='products_profile_base'),
 
     # AI추천받기: 영양제 추천 목록(나이 & 성별 기반)
     # menu: ai 영양제 추천받기(profile_info) > 영양 성분 리포트 > 영양제 추천 목록 
     # menu: home main(나이 & 성별 기반) > 영양제 추천 목록
     # /recommends/{profile-id}/surveys/{survey-id}/rec-collabo-products/
-    path('profile-id/surveys/survey-id/rec-collabo-products/', views.recom_products_collabo_base, name='products_collabo_base')
+    path('profile_id/surveys/survey_id/rec_collabo_products/', views.recom_products_collabo_base, name='products_collabo_base')
 ]
