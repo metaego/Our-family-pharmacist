5537aa9c11521975e9d278d7e1875155c1d8e039
Suryun Park <suryunpark23@gmail.com>
2024-04-27 17:34:18 +0900

    edit mypages templates

--- ourFamVita/mypages/urls.py
+++ ourFamVita/mypages/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'mypages'
 
 urlpatterns = [
-    path('<int:pk>/', views.mypage_main, name='mypage_main'),
-    path('<int:pk>/views/', views.mypage_views, name='mypage_views'),
-    path('<int:pk>/recommends/', views.mypage_recommends, name='mypage_recommends'), 
-    path('<int:pk>/likes/', views.mypage_likes, name='mypage_likes'), 
-    path('<int:pk>/reviews/', views.mypage_reviews, name='mypage_reviews'),  
+    path('<int:pk>/', views.mypage_main, name='main'),
+    path('<int:pk>/views/', views.mypage_views, name='views'),
+    path('<int:pk>/recommends/', views.mypage_recommends, name='recommends'), 
+    path('<int:pk>/likes/', views.mypage_likes, name='likes'), 
+    path('<int:pk>/reviews/', views.mypage_reviews, name='reviews'),  
     ]
