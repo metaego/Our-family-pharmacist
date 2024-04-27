68e9a4464e586d3c1bb42737caef85188d614f33
Suryun Park <suryunpark23@gmail.com>
2024-04-27 15:38:20 +0900

    edit models.py (user, db column, datetimefield), mypages, groups

--- ourFamVita/ourFamVita/settings.py
+++ ourFamVita/ourFamVita/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ea%%5i6&ill1g(s@i#4p$c0u*6n9z-kd@0&gzc$rj&hwps2@8=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
--- ourFamVita/users/models.py
+++ ourFamVita/users/models.py
@@ -13,22 +13,22 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from django.utils import timezone
+# from django.utils import timezone
 # decimalfield 음수 허용 불가, 리뷰 제한을 위한 MinValueValidator 임포트
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 
 class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    user_email = models.CharField(unique=True, max_length=50)
-    user_password = models.TextField()
-    user_role = models.CharField(max_length=20)
-    user_status = models.CharField(max_length=10)
-    created_at = models.DateTimeField(default=timezone.now)
-    modified_at = models.DateTimeField(blank=True, null=True)
-    deleted_at = models.DateTimeField(blank=True, null=True)
-    last_login = models.DateTimeField(default=timezone.now, blank=True, null=True)
+    custom_user_id = models.AutoField(primary_key=True)
+    custom_user_email = models.CharField(unique=True, max_length=50)
+    custom_user_password = models.TextField()
+    custom_user_role = models.CharField(max_length=20)
+    custom_user_status = models.CharField(max_length=10)
+    custom_created_at = models.DateTimeField(auto_now_add=True)
+    custom_modified_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    cumstom_deleted_at = models.DateTimeField(blank=True, null=True)
+    custom_last_login = models.DateTimeField(auto_now=True, blank=True, null=True)
 
 
     class Meta:
@@ -39,12 +39,12 @@ class User(models.Model):
 
 class Profile(models.Model):
     profile_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    custom_user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='custom_user_id')
     profile_name = models.CharField(max_length=20)
     profile_birth = models.DateField()
     profile_status = models.CharField(max_length=10)
-    created_at = models.DateTimeField(default=timezone.now)
-    modified_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True, blank=True, null=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
 
 
@@ -139,7 +139,7 @@ class Ingredient(models.Model):
 
 class IngredientFunction(models.Model):
     ingredient_function_id = models.AutoField(primary_key=True)
-    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE, db_column='ingredient_id')
     function_code = models.ForeignKey(FunctionCode, on_delete=models.CASCADE, db_column='function_code')
 
 
@@ -151,8 +151,8 @@ class IngredientFunction(models.Model):
 
 class Survey(models.Model):
     survey_id = models.BigAutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    custom_user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='custom_user_id')
+    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE, db_column='profile_id')
     survey_age_group = models.CharField(max_length=20)
     survey_sex = models.CharField(max_length=1)
     survey_pregnancy_code = models.CharField(max_length=10)
@@ -161,7 +161,7 @@ class Survey(models.Model):
     survey_smoke = models.CharField(max_length=1, blank=True, null=True)
     survey_height = models.DecimalField(max_digits=3, decimal_places=1, blank=True, null=True, validators=[MinValueValidator(0)])
     survey_weight = models.DecimalField(max_digits=3, decimal_places=1, blank=True, null=True, validators=[MinValueValidator(0)])
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
     class Meta:
@@ -172,7 +172,7 @@ class Survey(models.Model):
 
 class SurveyAllergy(models.Model):
     survey_allergy_id = models.BigAutoField(primary_key=True)
-    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE, db_column='survey_id')
     allergy_code = models.ForeignKey(AllergyCode, on_delete=models.CASCADE, db_column='allergy_code')
 
     class Meta:
@@ -182,7 +182,7 @@ class SurveyAllergy(models.Model):
 
 class SurveyDisease(models.Model):
     survey_disease_id = models.BigAutoField(primary_key=True)
-    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE, db_column='survey_id')
     disease_code = models.ForeignKey(DiseaseCode, on_delete=models.CASCADE, db_column='disease_code')
 
 
@@ -194,7 +194,7 @@ class SurveyDisease(models.Model):
 
 class SurveyFunction(models.Model):
     survey_function_id = models.BigAutoField(primary_key=True)
-    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE, db_column='survey_id')
     function_code = models.ForeignKey(FunctionCode, on_delete=models.CASCADE, db_column='function_code')
     # function_code가 '해당 없음'일 경우 survey_function_rank를 null로 주기로 결정해서 모델 스키마 재 정의(2024.04.26)
     # survey_function_rank = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
@@ -228,7 +228,7 @@ class Product(models.Model):
 
 class ProductFunction(models.Model):
     product_function_id = models.BigAutoField(primary_key=True)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, db_column='product_id')
     function_code = models.ForeignKey(FunctionCode,  on_delete=models.CASCADE, db_column='function_code')
 
     class Meta:
@@ -239,8 +239,8 @@ class ProductFunction(models.Model):
 
 class ProductIngredient(models.Model):
     product_ingredient_id = models.BigAutoField(primary_key=True)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    ingredient_id = models.ForeignKey(Ingredient,  on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, db_column='product_id')
+    ingredient_id = models.ForeignKey(Ingredient,  on_delete=models.CASCADE, db_column='ingredient_id')
 
 
     class Meta:
@@ -251,13 +251,13 @@ class ProductIngredient(models.Model):
 
 class ProductReview(models.Model):
     product_review_id = models.AutoField(primary_key=True)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE)
-    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE) 
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, db_column='product_id')
+    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE, db_column='survey_id')
+    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE, db_column='profile_id') 
     product_review_rating = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     product_review_content = models.CharField(max_length=200)
-    created_at = models.DateTimeField(default=timezone.now)
-    modified_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True, blank=True, null=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
 
 
@@ -269,10 +269,10 @@ class ProductReview(models.Model):
 
 class ProductLike(models.Model):
     product_like_id = models.AutoField(primary_key=True)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE) 
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=timezone.now)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, db_column='product_id')
+    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE, db_column='profile_id') 
+    custom_user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='custom_user_id')
+    created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
 
 
@@ -284,11 +284,11 @@ class ProductLike(models.Model):
 
 class ProductLog(models.Model):
     product_log_id = models.AutoField(primary_key=True)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE)
-    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    visited_at = models.DateTimeField(blank=True, null=True)
-    leaved_at = models.DateTimeField(blank=True, null=True)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, db_column='product_id')
+    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE, db_column='survey_id')
+    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE, db_column='profile_id')
+    visited_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    leaved_at = models.DateTimeField(auto_now=True, blank=True, null=True)
     product_log_duration = models.PositiveIntegerField(blank=True, null=True)
 
 
@@ -300,11 +300,15 @@ class ProductLog(models.Model):
 
 class Recommendation(models.Model):
     recommendation_id = models.BigAutoField(primary_key=True)
-    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=timezone.now)
-    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    custom_user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='custom_user_id', related_name='recommendations_user')
+    survey_id = models.ForeignKey(Survey, on_delete=models.CASCADE, db_column='survey_id')
+    created_at = models.DateTimeField(auto_now_add=True)
+    profile_id = models.ForeignKey(Profile, on_delete=models.CASCADE, db_column='profile_id')
+    
 
-
+# related_name='recommendations'이 설정하면
+# User 모델에서 Recommendation 모델로 역참조할 때 recommendations라는 속성을 사용할 수 있음
+# user.recommendations와 같이 사용하여 사용자와 관련된 모든 추천을 가져올 수 있음
     class Meta:
         
         db_table = 'recommendation'
@@ -313,8 +317,8 @@ class Recommendation(models.Model):
 
 class RecommendationIngredient(models.Model):
     recommendation_ingredient_id = models.BigAutoField(primary_key=True)
-    recommendation_id = models.ForeignKey(Recommendation,  on_delete=models.CASCADE)
-    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recommendation_id = models.ForeignKey(Recommendation,  on_delete=models.CASCADE, db_column='recommendation_id')
+    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE, db_column='ingredient_id')
 
     class Meta:
         
@@ -324,8 +328,8 @@ class RecommendationIngredient(models.Model):
 
 class RecommendationProduct(models.Model):
     recommendation_product_id = models.BigAutoField(primary_key=True)
-    recommendation_id = models.ForeignKey(Recommendation, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    recommendation_id = models.ForeignKey(Recommendation, on_delete=models.CASCADE, db_column='recommendation_id')
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, db_column='product_id')
 
 
     class Meta:
