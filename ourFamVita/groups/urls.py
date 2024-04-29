b71bbd92e3bb50397c332aa986c0faa632e6b14e
Suryun Park <suryunpark23@gmail.com>
2024-04-29 13:57:43 +0900

    edit groups urls and views.py

--- ourFamVita/groups/urls.py
+++ ourFamVita/groups/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'groups'
 
 urlpatterns = [
-    path('rec-nut-products/<int:user_id>/', views.group_main, name='group_main'),
-    path('rec-total-products/<int:user_id>/', views.group_detail, name='group_detail'),
-    path('rec-nut-products/<int:user_id>/<int:ingredient_id>/', views.group_individual, name='group_individual'), 
+    path('rec-nut-products/<int:profile_id>/', views.group_main, name='group_main'),
+    path('rec-total-products/<int:profile_id>/', views.group_detail, name='group_detail'),
+    path('rec-nut-products/<int:profile_id>/<int:ingredient_id>/', views.group_individual, name='group_individual'), 
     
     ]
