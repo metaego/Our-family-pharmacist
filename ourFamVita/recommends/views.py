a735d6c466cf4b7d67dba3ce05271eb89c11e11b
sapjil <ksy7678@gmail.com>
2024-05-16 23:58:45 +0900

    Refactor: Update client_ip handling and replace 'None' with '없음'

--- ourFamVita/recommends/views.py
+++ ourFamVita/recommends/views.py
@@ -226,10 +226,8 @@ def request_flask_recom_model(request, profile_id, survey_id):
     
     client_ip = request.META.get('REMOTE_ADDR', None)
 
-    if client_ip == '127.0.0.1':
-        pass
-    elif client_ip != os.environ.get('AWS_PUBLIC_IP'):
-         client_ip = os.environ.get('AWS_PUBLIC_IP')
+    if client_ip != '127.0.0.1' and client_ip != os.environ.get('AWS_PUBLIC_IP'):
+        client_ip = os.environ.get('AWS_PUBLIC_IP')
     csrf_token = get_token(request)
     survey = Survey.objects.filter(profile_id=profile_id).latest('created_at')
     # print(f'최근 survey_id: {survey.survey_id}')
@@ -675,10 +673,8 @@ def request_flask_collabo_recom_model(request, profile_id):
     # flask 요청 
     client_ip = request.META.get('REMOTE_ADDR', None)
 
-    if client_ip == '127.0.0.1':
-        pass
-    elif client_ip != os.environ.get('AWS_PUBLIC_IP'):
-         client_ip = os.environ.get('AWS_PUBLIC_IP')
+    if client_ip != '127.0.0.1' and client_ip != os.environ.get('AWS_PUBLIC_IP'):
+        client_ip = os.environ.get('AWS_PUBLIC_IP')
     
     csrf_token = get_token(request)
     response = requests.post('http://' + client_ip + f':5000/ai-collabo-recom/{survey.survey_id}', 
