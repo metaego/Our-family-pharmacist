4440226ae9802579a0758c460c476dd876aa0d75
silverA-01 <silvermare01@gmail.com>
2024-06-28 14:33:10 +0900

    feat: add new directory and file about ingredeint data processing

---
+++ data_processing/com_code.py
@@ -0,0 +1,16 @@
+com_code_grp_dict = {'FUNCTION' : ['HF'], 'DISEASE' : ['DI'], 'ALLERGY' : ['AL'],
+                     'OPERATION' : ['O'], 'PREGNANCY' : ['P'], 'SMOKING' : ['S'],
+                     'AGEGROUP' : ['INFANT_TODDLER', 'KIDS', 'TEENAGER', 'ADULT', 'OLD'], 'SEX' : ['FEMALE', 'MALE']}
+
+com_code_columns = ['HF00', 'HF01', 'HF02', 'HF03', 'HF04', 'HF05', 'HF06', 'HF07', 'HF08', 'HF09', 'HF10', 
+                    'HF11', 'HF12', 'HF13', 'HF14', 'HF15', 'HF16', 'HF17', 'HF18', 'HF19', 'HF20', 'HF21', 'HF22', 'HF23', 'HF24', 'HF25', 'HF26',  
+                    'DI01', 'DI02', 'DI03', 'DI04', 'DI05', 'DI06', 'DI07', 'DI08', 'DI09', 'DI10', 
+                    'DI11', 'DI12', 'DI13', 'DI14', 'DI15', 'DI16', 'DI17', 
+                    'AL01', 'AL02', 'AL03', 'AL04', 'AL05', 'AL06', 'AL07', 'AL08', 'AL09', 'AL10', 
+                    'AL11', 'AL12', 'AL13', 'AL14', 'AL15', 'AL16', 'AL17', 'AL18', 'AL19', 'AL20', 
+                    'P3', 'P2', 'P1', 
+                    'INFANT_TODDLER', 'KIDS', 'TEENAGER', 'ADULT', 'OLD', 
+                    'FEMALE', 'MALE', 
+                    'S1', 
+                    'O3', 'O2', 'O1', 
+                    '단백질', '비타민 A', '베타카로틴', '요오드']
