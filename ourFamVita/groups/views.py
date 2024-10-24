93b7b256567abb7551d6ebbb16d7f632f1dbae6f
silverA-01 <silvermare01@gmail.com>
2024-10-24 14:32:25 +0900

    refactor: update mypages and groups app

--- ourFamVita/groups/views.py
+++ ourFamVita/groups/views.py
@@ -1,49 +1,99 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from users.models import (Profile, Product, ProductView, ProductLike, ProductReview
+                          , ProductIngredient
+                          , Survey, SurveyComCode
+                          , ComCode
+                          , Ingredient  
+                          , Recom, RecomIngredient, RecomSurveyProduct
+                          , User
+                          )
+from django.db.models import OuterRef, Subquery
 # from users.models import User, Profile, Recommendation, ProductIngredient, Product, ProductLike, RecommendationIngredient, RecommendationProduct, Ingredient, SurveyFunction, FunctionCode
 
 # Create your views here.
 def group_main(request, profile_id):
-    profile = Profile.objects.get(profile_id = profile_id)
-    user = User.objects.get(custom_user_id = profile.custom_user_id.custom_user_id)
-    recommendations_info = Recommendation.objects.filter(custom_user_id=user.custom_user_id).order_by('-created_at')
- 
+    profile = Profile.objects.get(profile_id=profile_id)
+    user = User.objects.get(user_id=profile.user_id.user_id)
+    recommendations_info = Recom.objects.filter(user_id=user.user_id).order_by('-recom_created_at')[:50]
+    
+    # 가족 최근 건강고민
+    check_profile = {}
+    for recom in recommendations_info:
+        if recom.profile_id not in check_profile:
+            check_profile[recom.profile_id.profile_id] = 0
+            
+    print(check_profile)
+    
+    latest_surveys = []
+    for profile_id in check_profile.keys():
+        latest_profile_recoms = Recom.objects.filter(profile_id=profile_id).order_by('-recom_created_at')[:3]
+        for latest_profile_recom in latest_profile_recoms:
+            latest_surveys.append(latest_profile_recom.survey_id)
+    print(latest_surveys) 
+    
+    check_function_dict = {}
+    check_functions = []
+    for survey in latest_surveys:
+        print(survey, survey.survey_function_code.values())
+        for function_code in survey.survey_function_code.values():
+            if function_code != 'HF00':
+                if function_code not in check_functions:
+                    check_functions.append(function_code)
+                    check_function_dict[function_code] = 1
+                else:
+                    check_function_dict[function_code] += 1
+    check_function_dict = dict(sorted(check_function_dict.items(), key=lambda x: x[1], reverse=True))
+    
+    group_latest_functions = list(check_function_dict.keys())[:5]
+    
+    #
+    
     functions_list = []
     ingredients_list = []
     products_list = []
+    
+    for f in group_latest_functions:
+        functions = ComCode.objects.get(com_code=f)
+        functions_list.append(functions)
+        
+    print(functions_list)
+    #
 
     for recommendation in recommendations_info:
-        # user 건강고민
-        survey_functions = SurveyFunction.objects.filter(survey_id = recommendation.survey_id.survey_id)[:6]
-        for survey_function in survey_functions:
-            function_code = FunctionCode.objects.get(function_code = survey_function.function_code.function_code)
-            if function_code.function_code != 'HF00':
-                functions_list.append(function_code)
+        # # user 건강고민
+        # survey_functions = SurveyComCode.objects.filter(com_code_grp='FUNCTION').filter(survey_id=recommendation.survey_id.survey_id)[:6]
+        # for survey_function in survey_functions:
+        #     function_code = ComCode.objects.get(com_code=survey_function.com_code.com_code)
+        #     if function_code.com_code != 'HF00':
+        #         functions_list.append(function_code)
+        
 
         # user 추천 영양 성분
-        recommendation_ingredients = RecommendationIngredient.objects.filter(recommendation_id = recommendation.recommendation_id)[:3]
+        recommendation_ingredients = RecomIngredient.objects.filter(recom_id = recommendation.recom_id)[:3]
         for recommendation_ingredient in recommendation_ingredients: 
             ingredient = Ingredient.objects.get(ingredient_id = recommendation_ingredient.ingredient_id.ingredient_id)
             ingredients_list.append(ingredient)
 
         # user 추천 영양제
-        recommendation_products = RecommendationProduct.objects.filter(recommendation_id = recommendation.recommendation_id)[:3]
+        recommendation_products = RecomSurveyProduct.objects.filter(recom_id = recommendation.recom_id)[:3]
         for recommendation_product in recommendation_products: 
             product = Product.objects.get(product_id = recommendation_product.product_id.product_id)
             products_list.append(product)
         
-
+    
     # user 좋아요 영양제
-    product_likes = ProductLike.objects.filter(custom_user_id=user.custom_user_id).order_by('-created_at')[:3]   
+    product_likes = ProductLike.objects.filter(user_id=user.user_id).order_by('-product_like_created_at')[:3]   
     product_like_list = []
     for product_like in product_likes:
         product = Product.objects.get(product_id = product_like.product_id.product_id)
         product_like_list.append(product) 
       
     context = { 'profile':profile,
-                'functions_list': functions_list[:3],
+                'functions_list': functions_list,
                 'ingredients_list': ingredients_list[:3],
                 'products_list' : products_list[:3],
-                'product_like_list' : product_like_list,                                 
+                'product_like_list' : product_like_list,  
+                'group_latest_functions' : group_latest_functions                               
                 } 
    
     return render(request, 'groups/main.html', context)
@@ -51,12 +101,12 @@ def group_main(request, profile_id):
 
 def group_detail(request, profile_id):
     profile = Profile.objects.get(profile_id = profile_id)
-    user = User.objects.get(custom_user_id = profile.custom_user_id.custom_user_id)
-    recommendations_info = Recommendation.objects.filter(custom_user_id=user.custom_user_id).order_by('-created_at').first()   
+    user = User.objects.get(user_id = profile.user_id.user_id)
+    recommendations_info = Recom.objects.filter(user_id=user.user_id).order_by('-recom_created_at').first()   
     
     # user 추천 영양제
     products_list = []
-    recommendation_products = RecommendationProduct.objects.filter(recommendation_id = recommendations_info.recommendation_id)[:5]
+    recommendation_products = RecomSurveyProduct.objects.filter(recom_id = recommendations_info.recom_id)[:5]
     for recommendation_product in recommendation_products:
         product = Product.objects.get(product_id = recommendation_product.product_id.product_id)
         products_list.append(product)
@@ -68,14 +118,14 @@ def group_detail(request, profile_id):
 
 
 def group_individual(request, profile_id, ingredient_id):
-    profile = Profile.objects.get(profile_id = profile_id)
+    profile = Profile.objects.get(profile_id=profile_id)
     ingredient = Ingredient.objects.get(ingredient_id=ingredient_id)
     product_ingredients = ProductIngredient.objects.filter(ingredient_id=ingredient_id)[:5]
     products_list = []
     for product_ingredient in product_ingredients:
 
         # user 추천 영양제
-        product = Product.objects.get(product_id = product_ingredient.product_id.product_id)
+        product = Product.objects.get(product_id=product_ingredient.product_id.product_id)
         products_list.append(product)
         
     context = { 'profile':profile,
--- ourFamVita/mypages/views.py
+++ ourFamVita/mypages/views.py
@@ -1,11 +1,17 @@
+import json
 from django.shortcuts import render, redirect, get_object_or_404
 # from users.models import Profile, ProductLog, Product, Recommendation, RecommendationIngredient, Ingredient, SurveyFunction, ProductLike, ProductReview, FunctionCode
-
+from users.models import (Profile, Product, ProductView, ProductLike, ProductReview
+                          , Survey
+                          , ComCode
+                          , Ingredient  
+                          , Recom, RecomIngredient
+                          )
 
 # Create your views here.
 
 def mypage_main(request, profile_id):
-    profile = get_object_or_404(Profile, profile_id=profile_id)
+    profile = get_object_or_404(Profile, profile_id=profile_id)   
     context = {'profile': profile,
                   }
     return render(request, 'mypages/main.html', context)
@@ -14,11 +20,14 @@ def mypage_main(request, profile_id):
 
 def mypage_views(request, profile_id):
     profile = get_object_or_404(Profile, profile_id=profile_id)
-    productlogs = ProductLog.objects.filter(profile_id=profile_id).order_by('-visited_at')[:5] 
+    # productlogs = ProductLog.objects.filter(profile_id=profile_id).order_by('-visited_at')[:5] 
+    product_views = ProductView.objects.filter(profile_id=profile_id).order_by('-product_view_visited_at')[:5] 
     products_list = []
 
-    for productlog in productlogs: 
-        product = Product.objects.get(product_id = productlog.product_id.product_id)
+    # for productlog in productlogs: 
+    for product_view in product_views:
+        # product = Product.objects.get(product_id = productlog.product_id.product_id)
+        product = Product.objects.get(product_id = product_view.product_id.product_id)
         products_list.append(product)
     context =  {'profile': profile, 
                 'products_list':products_list,
@@ -28,42 +37,63 @@ def mypage_views(request, profile_id):
 
 def mypage_recommends(request, profile_id):
     profile = get_object_or_404(Profile, profile_id=profile_id)     
-    recommendations_info = Recommendation.objects.filter(profile_id=profile_id).order_by('-created_at')[:5]
+    # recommendations_info = Recommendation.objects.filter(profile_id=profile_id).order_by('-created_at')[:5]
+    recoms_info = Recom.objects.filter(profile_id=profile_id).order_by('-recom_created_at')[:5] # 최근 5개 추천 목록 
     ingredients_list = []
 
-    for recommendation in recommendations_info: 
+    # for recommendation in recommendations_info: 
+    for recom in recoms_info: 
         ingredients_info = []
         functions_info = []
-        recommendation_ingredients = RecommendationIngredient.objects.filter(recommendation_id=recommendation.recommendation_id)
+        # recommendation_ingredients = RecommendationIngredient.objects.filter(recommendation_id=recommendation.recommendation_id)
+        recom_ingredients = RecomIngredient.objects.filter(recom_id=recom.recom_id)
         
-        for recommendation_ingredient in recommendation_ingredients:
-            ingredient = Ingredient.objects.get(ingredient_id=recommendation_ingredient.ingredient_id.ingredient_id)
+        # for recommendation_ingredient in recommendation_ingredients:
+        for recom_ingredient in recom_ingredients:
+            # ingredient = Ingredient.objects.get(ingredient_id=recommendation_ingredient.ingredient_id.ingredient_id)
+            ingredient = Ingredient.objects.get(ingredient_id=recom_ingredient.ingredient_id.ingredient_id)
             ingredients_info.append(ingredient)
-
-        survey_functions = SurveyFunction.objects.filter(survey_id=recommendation.survey_id) 
-
-        for survey_function in survey_functions:
-            function_code = FunctionCode.objects.get(function_code = survey_function.function_code.function_code)
-            functions_info.append(function_code)
-            
-        info = [ingredients_info, functions_info, recommendation]
+        
+        # survey에서 function code 불러오기  ----------------------------------------  
+        # survey_functions = SurveyFunction.objects.filter(survey_id=recommendation.survey_id) 
+        survey = Survey.objects.get(survey_id=recom.survey_id.survey_id)
+        survey_function_code_dict = survey.survey_function_code # {'1st': 'HF07', '2nd': 'HF08'}
+        
+        # functions_info = list(survey_function_code_dict.values())  # ['HF07', 'HF08']
+        
+        for function_code in survey_function_code_dict.values():
+            function_code_name = ComCode.objects.get(com_code=function_code).com_code_name
+            functions_info.append(function_code_name)
+        print(f'functions_info:{functions_info}')    
+        # for survey_function in survey_functions:
+        #     function_code = FunctionCode.objects.get(function_code = survey_function.function_code.function_code)
+        #     functions_info.append(function_code)
+    
+        # info = [ingredients_info, functions_info, recommendation]
+        info = [ingredients_info, functions_info, recom]
+        
         ingredients_list.append(info)
 
     context = {
         'profile': profile,
         'ingredients_list': ingredients_list,
+        # 'survey': survey,
     }
     return render(request, 'mypages/recommends.html', context)
 
-
+# 영양제 찜하기 기능 구현 전
 def mypage_likes(request, profile_id):
     profile = get_object_or_404(Profile, profile_id=profile_id) 
-    product_likes = ProductLike.objects.filter(profile_id=profile_id).order_by('-created_at')[:5]
+    # print(f'profile:{profile}')
+    # product_likes = ProductLike.objects.filter(profile_id=profile_id).order_by('-created_at')[:5]
+    product_likes = ProductLike.objects.filter(profile_id=profile_id).order_by('-product_like_created_at')[:5]
+    # print(f'product_likes:{product_likes}')
     products_list = []
     for product_like in product_likes:
         product = Product.objects.get(product_id = product_like.product_id.product_id)
+        # print(f'product:{product}')
         products_list.append(product)
-    
+    # print(f'products_list:{products_list}')
     context = {'profile': profile,
                'products_list':products_list,                                
                   }
@@ -72,7 +102,8 @@ def mypage_likes(request, profile_id):
 
 def mypage_reviews(request, profile_id):
     profile = get_object_or_404(Profile, profile_id=profile_id) 
-    product_reviews = ProductReview.objects.filter(profile_id=profile_id).order_by('-created_at')[:5]
+    # product_reviews = ProductReview.objects.filter(profile_id=profile_id).order_by('-created_at')[:5]
+    product_reviews = ProductReview.objects.filter(profile_id=profile_id).order_by('-product_review_created_at')[:5]
     products_list = []
     for product_review in product_reviews:
         product = Product.objects.get(product_id = product_review.product_id.product_id)
