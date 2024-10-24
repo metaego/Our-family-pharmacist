2b52e55c6bfb1e40cf24df2f3c91a719efbaae22
silverA-01 <silvermare01@gmail.com>
2024-10-24 14:23:47 +0900

    feat: add DB port configuration for .env updates

--- ourFamVita/ourFamVita/settings.py
+++ ourFamVita/ourFamVita/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'USER': os.environ.get('MYSQL_USER'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
         'HOST': os.environ.get('MYSQL_HOST'),
-        'PORT': 3306,
+        'PORT': int(os.environ.get('MYSQL_PORT')),
     }
 } 
 
